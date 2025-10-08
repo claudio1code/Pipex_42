86e5e4a00471bc48422d7d0c609cb0adb0209db8
Claudio Matheus <claudio1.code@gmail.com>
2025-10-08 15:56:40 -0300

    feat: implementação do pipe e do fork

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:59:24 by clados-s          #+#    #+#             */
-/*   Updated: 2025/10/08 15:25:08 by clados-s         ###   ########.fr       */
+/*   Updated: 2025/10/08 15:56:10 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ void	init_data(t_pipex *data, int argc, char **argv)
 	data->infile = argv[1];
 	data->outfile = argv[4];
 	data->cmd1_args = ft_split(argv[2], ' ');
-	data->cmd1_args = ft_split(argv[3], ' ');
+	data->cmd2_args = ft_split(argv[3], ' ');
 }
 
 int	main(int argc, char **argv)
 {
 	t_pipex	data;
+	int		pipe_fd[2];
+	
 
 	if (argc != 5)
 	{
@@ -30,4 +32,8 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	init_data(&data, argc, argv);
+	pipe(pipe_fd[2]);
+	if (pipe_fd[2] == -1)
+		return (0);
+	fork()
 }
